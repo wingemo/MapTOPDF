f31374167552086c3f7fb444ad155d265c0f899a
Wingemo <philip.wingemo@outlook.com>
2020-10-26 14:42:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,29 +12,30 @@ from ttkthemes import ThemedStyle
 
 class App(tk.Frame):
     def __init__(self, master):
+        """The main class that handles the GUI"""
+        
         super().__init__(master)
         self.pack()
         
+        # and split into a list of lines:
+        self.progress = ttk.Progressbar(root, length = 100, mode = 'indeterminate')
+        self.progress.place(x = 0, y = 40, height = 15, width = 620)
+        
+        # and split into a list of lines:
         self.w = ttk.Button(text ="Merge", command = self.thread_start)
         self.w.place(x = 629, y = 0, height = 35)
 
+        # and split into a list of lines:
         self.entrythingy = ttk.Entry(font = "Calibri 12")
         self.entrythingy.place(x = 0, y = 0, height = 35, width = 620)
-
         self.contents = tk.StringVar()
-
         self.entrythingy["textvariable"] = self.contents
-
         self.entrythingy.bind('<Key-Return>', self.print_contents)
-
         self.entrythingy.event_add('<<Paste>>', '<Button-3>')
-
-        self.progress = ttk.Progressbar(root, length = 100, mode = 'indeterminate')
-        self.progress.place(x = 0, y = 40, height = 15, width = 620)
-
         self.entrythingy.insert(0, "Klistra in sökvägen")
         self.entrythingy.configure(state="DISABLED")
         self.entrythingy.bind('<Button-1>', self.on_click)
+        self.entrythingy.bind('<Button-3>', self.on_click)
 
     def on_click(self, master):
         """Deletes placeholder for input"""
@@ -42,6 +43,7 @@ class App(tk.Frame):
         self.entrythingy.delete(0, "")
     
     def thread_start(self):
+        
         thread.start_new_thread(ConvertHanlder(self))
 
     def exitProgram(self):
