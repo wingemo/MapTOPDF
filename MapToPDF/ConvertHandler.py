c193a7f926d3688454466fe1bb669abaf04db69e
Wingemo <philip.wingemo@outlook.com>
2020-10-27 11:18:15 +0100

    Update ConvertHandler.py

--- MapToPDF/ConvertHandler.py
+++ MapToPDF/ConvertHandler.py
@@ -51,17 +51,25 @@ class ConvertHandler:
        self.progress['value'] = value
        self.progress.update()
 
-   def files_to_pdf(self): 
+   def word_to_pdf(self):
        """Deletes placeholder for input"""
+       convert(path + "\\" + filename)
+
+   def image_to_pdf(self):
+       """Deletes placeholder for input"""
+       image1 = Image.open(path + "\\" + filename)
+       im1 = image1.convert('RGB')
+       im1.save(path + "\\" + filename + ".pdf")
+
+   def files_to_pdf(self): 
+        """Deletes placeholder for input"""
         try:
             for filename in os.listdir(path):
                 if filename.endswith(".docx") or filename.endswith(".doc"):
-                    convert(path + "\\" + filename)
+                    multiprocessing.Process(target=word_to_pdf, args=(pdf))
                     continue
                 elif filename.endswith(".jpg") or filename.endswith(".PNG")  or filename.endswith(".png"):
-                    image1 = Image.open(path + "\\" + filename)
-                    im1 = image1.convert('RGB')
-                    im1.save(path + "\\" + filename + ".pdf")
+                    multiprocessing.Process(target=image_to_pdf, args=(pdf))
                     continue
                 else:
                     continue
