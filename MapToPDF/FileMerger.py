22db515364c2d68df179aeb4f1395e9d92adfa29
Wingemo <philip.wingemo@outlook.com>
2020-10-27 11:47:38 +0100

    Update FileMerger.py

--- MapToPDF/FileMerger.py
+++ MapToPDF/FileMerger.py
@@ -1,1 +1,16 @@
+  """Deletes placeholder for input"""
+        open_pdf = []
+        x = [a for a in os.listdir(path) if a.endswith(".pdf")]
+        merger = PdfFileMerger()
 
+        for pdf in x:
+            f = open(path + "\\"+ pdf, 'rb')
+            open_pdf.append(f)
+            merger.append(f)
+
+        with open(path + "\\"+ PDF_OUTPUT_NAME, "wb") as fout:
+            merger.write(fout)
+
+        for pdf in open_pdf:
+            pdf.close()
+            merger.close()
