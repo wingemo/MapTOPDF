06bca96d389eed81f30949e128c2472836d1eaea
Wingemo <philip.wingemo@outlook.com>
2020-10-27 12:07:55 +0100

    Update FileConverter.py

--- MapToPDF/FileConverter.py
+++ MapToPDF/FileConverter.py
@@ -1,14 +1,12 @@
+class FileConverter:
+""" This class provides utility functions"""
 
+   MESSAGEBOX_TITLE = "INFO"
+   SUCCESS_MESSAGE = "All files have been compiled into a pdf - Philip"
 
-   def word_to_pdf(self):
-       """Deletes placeholder for input"""
-       convert(path + "\\" + filename)
-
-   def image_to_pdf(self):
-       """Deletes placeholder for input"""
-       image1 = Image.open(path + "\\" + filename)
-       im1 = image1.convert('RGB')
-       im1.save(path + "\\" + filename + ".pdf")
+   def __init__(self, path):
+        """Deletes placeholder for input"""
+        files_to_pdf(self)
 
    def files_to_pdf(self): 
         """Deletes placeholder for input"""
@@ -24,3 +22,13 @@
                     continue
         except Exception as e:
             messagebox.showerror("Info", "Error" + str(e))
+   
+   def word_to_pdf(self):
+       """Deletes placeholder for input"""
+       convert(path + "\\" + filename)
+
+   def image_to_pdf(self):
+       """Deletes placeholder for input"""
+       image1 = Image.open(path + "\\" + filename)
+       im1 = image1.convert('RGB')
+       im1.save(path + "\\" + filename + ".pdf")
