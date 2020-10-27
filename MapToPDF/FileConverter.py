2f72f8db99777edfb9dcf580aa016c0bd6b94554
Wingemo <philip.wingemo@outlook.com>
2020-10-27 11:43:34 +0100

    Update FileConverter.py

--- MapToPDF/FileConverter.py
+++ MapToPDF/FileConverter.py
@@ -1,1 +1,26 @@
 
+
+   def word_to_pdf(self):
+       """Deletes placeholder for input"""
+       convert(path + "\\" + filename)
+
+   def image_to_pdf(self):
+       """Deletes placeholder for input"""
+       image1 = Image.open(path + "\\" + filename)
+       im1 = image1.convert('RGB')
+       im1.save(path + "\\" + filename + ".pdf")
+
+   def files_to_pdf(self): 
+        """Deletes placeholder for input"""
+        try:
+            for filename in os.listdir(path):
+                if filename.endswith(".docx") or filename.endswith(".doc"):
+                    multiprocessing.Process(target=word_to_pdf, args=(pdf))
+                    continue
+                elif filename.endswith(".jpg") or filename.endswith(".PNG")  or filename.endswith(".png"):
+                    multiprocessing.Process(target=image_to_pdf, args=(pdf))
+                    continue
+                else:
+                    continue
+        except Exception as e:
+            messagebox.showerror("Info", "Error" + str(e))
